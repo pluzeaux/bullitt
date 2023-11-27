4cf907584bf2c06d53bf9569aa4b88fb98750e86
pluzeaux <philippe.luzeaux@gmail.com>
2023-11-27 23:36:33 +0100

    correct call to combine method

--- examples/combinatory.rs
+++ examples/combinatory.rs
@@ -6,14 +6,13 @@ fn main() {
     let set2: Vec<i32> = (1..=SIZE).collect();
     let _len: i32 = 4;
 
-    let set = ['a', 'b', 'c'];
+    let set = vec!['a', 'b', 'c'];
     let nb_element = set2.len();
     let nb_parties = u128::pow(2, nb_element as u32);
 
     let start: Instant = Instant::now();
 
-    let binding = set.to_vec();
-    let res = combine(&binding, nb_element.try_into().unwrap(), nb_parties);
+    let res = combine(&set, nb_element.try_into().unwrap(), nb_parties);
 
     let duration: Duration = start.elapsed();
     println!(
