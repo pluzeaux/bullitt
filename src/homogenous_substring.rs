78ed85ab73f26f2c0dbdfad4145f913c04d78018
pluzeaux <philippe.luzeaux@gmail.com>
2023-11-27 18:37:40 +0100

    cleaning code

--- src/homogenous_substring.rs
+++ src/homogenous_substring.rs
@@ -1,10 +1,9 @@
 pub fn count_homogenous(s: String) -> i32 {
     let vc: Vec<char> = s.chars().collect();
     let mut vvc: Vec<Vec<char>> = Vec::new();
-
+    
     let mut ls: Vec<char> = vec![vc[0]];
-    ls.push(vc[0]);
-
+    
     for i in 1..vc.len() {
         if vc[i - 1] != vc[i] {
             vvc.push(ls);
@@ -14,8 +13,7 @@ pub fn count_homogenous(s: String) -> i32 {
     }
     vvc.push(ls);
 
-    let res = vvc
-        .iter()
+    let res = vvc.iter()
         .map(|s| s.len() as i128 * (s.len() + 1) as i128 / 2)
         .sum::<i128>();
 
